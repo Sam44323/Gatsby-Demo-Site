2c402bb92ee95cb007b1ce5814f7bda77df40739
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-29 00:28:28 +0530

    error page created

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Error: React.FC = () => {
+  return (
+    <div>
+      <h2>404</h2>
+      <p>Sorry, that page doesn't exist</p>
+    </div>
+  )
+}
+
+export default Error
