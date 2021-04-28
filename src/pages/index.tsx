100e33c0a498c90b494189df8f0ab2e319888892
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-29 00:16:52 +0530

    added a new about page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
 import React from "react"
 
-const Home: React.FC = () => <h1>Hello from Gatsby</h1>
+const Home: React.FC = () => {
+  return (
+    <section>
+      <div>
+        <h2>Design</h2>
+        <h3>Develop & Deploy</h3>
+        <p>UX Designer & web developer based in Manchester</p>
+      </div>
+    </section>
+  )
+}
 
 export default Home
