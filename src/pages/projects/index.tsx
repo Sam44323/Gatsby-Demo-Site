f5576a53f9d8d81b79720691754457be055976b4
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-09 01:01:54 +0530

    stylings added for project displaying

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,4 +1,4 @@
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import * as styles from "../../styles/projects.module.css"
@@ -6,18 +6,27 @@ import { PortfolioInterface } from "../../utils/interfaces"
 
 const ProjectHome: React.FC<{
   data: { allMarkdownRemark: { nodes: PortfolioInterface[] } }
-}> = props => {
-  const {
-    data: {
-      allMarkdownRemark: { nodes: portfolio },
-    },
-  } = props
+}> = ({
+  data: {
+    allMarkdownRemark: { nodes: portfolio },
+  },
+}) => {
   console.log(portfolio)
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
+        <div className={styles.projects}>
+          {portfolio.map(project => (
+            <Link to={`/projects/${project.frontmatter.slug}`} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
