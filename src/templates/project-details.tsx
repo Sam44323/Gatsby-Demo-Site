e3199df41da56df184530ac6f41d1d9bdaf18d9a
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-09 18:05:50 +0530

    basic template layout added

---
+++ src/templates/project-details.tsx
@@ -0,0 +1,19 @@
+import Layout from "../components/Layout"
+import React from "react"
+import Img from "gatsby-image"
+import styles from "../styles/project-details.module.css"
+
+const ProjectDetails: React.FC = () => {
+  return (
+    <Layout>
+      <div className={styles.details}>
+        <h2>title</h2>
+        <h3>stack</h3>
+        <div className={styles.featured}>{/* <Img fluid={""} /> */}</div>
+        {/* <div dangerouslySetInnerHTML={} className={styles.html} /> */}
+      </div>
+    </Layout>
+  )
+}
+
+export default ProjectDetails
